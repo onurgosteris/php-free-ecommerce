e2098608c1a02611704bda4d0a1578bc23a150db
Onur Gösteriş <38893935+onurgosteris@users.noreply.github.com>
2021-03-22 22:01:22 +0300

    Add files via upload

---
+++ src/html/footer.php
@@ -0,0 +1,9 @@
+<?php 
+ob_start();
+session_start();
+require_once 'setconfig.php';
+ ?>
+<footer class="footer text-center">
+	Tüm Haklarımız Gizlidir. <a
+	href="https://www.instagram.com/onurgosteris/">Onur Gösteriş</a> Tarafından Oluşturulmuştur.
+</footer>
